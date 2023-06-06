af6ac4fe009ba12f9689fd5e089f6445a09d19bf
june hwang <zzang535@gmail.com>
2023-06-07 05:44:20 +0900

    api test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,30 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 const Home: NextPage = () => {
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://se0c53vznc.execute-api.ap-northeast-2.amazonaws.com/dev/test');
+        console.log(response)
+        setData(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    fetchData();
+  }, []);
+
+
   return (
-    <h1>Home Page</h1>
+    <>
+      <h1>marijuni 입니다.</h1>
+      <div>{ data }</div>
+    </>
   )
 }
 
