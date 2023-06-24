631abc15474933613d92d36ecd1b7a20a5b1db4a
june hwang <zzang535@gmail.com>
2023-06-24 21:39:55 +0900

    next link import 대소문자 오류 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Link from "next/Link";
+import Link from "next/link";
 import { AppBar, Toolbar, Typography, Button, createTheme, ThemeProvider } from '@material-ui/core'; // 스타일 
 import { makeStyles } from '@material-ui/core/styles'; // 스타일 객체
 
