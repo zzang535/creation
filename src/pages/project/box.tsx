80065e86eef3ab8fefbb8254888c67ab58cfe515
june hwang <zzang535@gmail.com>
2023-11-02 19:49:03 +0900

    push

--- src/pages/project/box.tsx
+++ src/pages/project/box.tsx
@@ -24,8 +24,8 @@ const Home: NextPage = () => {
     canvasRef.current.appendChild(renderer.domElement);
 
     // 큐브 생성
-    const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const geometry = new THREE.BoxGeometry(2, 2, 2);
+    const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
     const cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
 
